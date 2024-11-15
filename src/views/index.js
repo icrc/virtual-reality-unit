258e3fdfd14d1df2706992e125d880b870f8bb83
Jon Randy <jonrandy@gmail.com>
2024-11-15 14:55:40 +0700

    Remove an old test page

--- src/views/index.js
+++ src/views/index.js
@@ -1,7 +1,7 @@
 // All Main Views ----------------------------------------
 
 ////// export { default as NotFound } from "../NotFound.vue"
-export { default as NotFound } from "../App_old.vue"
+export { default as NotFound } from "./NotFound.vue"
 
 export { default as Home } from "./Home.vue"
 export { default as Edit } from "./Edit.vue"
