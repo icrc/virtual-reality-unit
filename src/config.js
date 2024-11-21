c4e0bcabef2ba453f650cdb44b15679cf9453119
Jon Randy <jonrandy@gmail.com>
2024-11-21 12:48:57 +0700

    Add video source types to config

--- src/config.js
+++ src/config.js
@@ -1,15 +1,20 @@
 // General System Config
-// 
-import { useStorage } from '@/libs/storage'
+//
+import { useStorage } from "@/libs/storage"
 // import { dropboxStorage } from '@/libs/storage/dropbox'
-import { testStorage } from '@/libs/storage/test'
+import { testStorage } from "@/libs/storage/test"
 
-export const VERSION = '0.0.1'
+export const VERSION = "0.0.1"
+
+export const VIDEO_SOURCE_TYPES = {
+	vimeo: { id: "vimeo", name: "Vimeo", features: { getChapters: true } },
+	mp4: { id: "mp4", name: "MP4", features: {} },
+}
+export const DEFAULT_VIDEO_SOURCE_TYPE = "vimeo"
 
 // storage
 // TODO - make this work with Dropbox
 // const storage = useStorage(dropboxStorage)
-// 
+//
 
 export const storage = await useStorage(testStorage)
-
