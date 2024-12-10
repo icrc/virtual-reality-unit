63c3298df19d2778cd54dc0c714cabdc232f4275
Jon Randy <jonrandy@gmail.com>
2024-12-10 14:39:48 +0700

    Add headers to popup functions

--- src/libs/popups.js
+++ src/libs/popups.js
@@ -4,14 +4,33 @@
 
 // for now just wrapping browser equivalents, but can move to something nicer later
 
+/**
+ * Show an alert message
+ *
+ * @param      {String}     [message=undefined]  Message to show
+ * @return     {undefined}  Returns nothing
+ */
 export const alert = async (message = undefined) => {
 	return window.alert(message)
 }
 
+/**
+ * Prompts user for an input value (string)
+ *
+ * @param      {String}         [message=undefined]       Message to show
+ * @param      {String}         [defaultValue=undefined]  Default value
+ * @return     {(String|Null)}  Entered value or null if cancelled
+ */
 export const prompt = async (message = undefined, defaultValue = undefined) => {
 	return window.prompt(message, defaultValue)
 }
 
+/**
+ * Show a confirmation prompt (with OK/Cancel)
+ *
+ * @param      {String}   [message=undefined]  Message to show
+ * @return     {Boolean}  true - OK, false - Cancel
+ */
 export const confirm = async (message = undefined) => {
 	return window.confirm(message)
 }
