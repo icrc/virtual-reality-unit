6467024359288728748d3914b21314b1f37ba23d
Jon Randy <jonrandy@gmail.com>
2024-12-10 14:40:04 +0700

    Skeleton of choices component

---
+++ src/components/Choices.vue
@@ -0,0 +1,32 @@
+<!-- Choices Component -->
+<template>
+
+<div class="choices_container">
+  <div :class="{ choice: true, [`choice_${index+1}`]: true }" v-for="(choice, index) in choices" :key="choice.id" @click="emit('choiceMade', choice)">
+    Choice {{ index+1 }}
+  </div>
+</div>
+
+</template>
+
+<script>
+
+</script>
+
+<script setup>
+import { ref } from "vue"
+
+const props = defineProps({
+  choices: {
+    type: Array,
+    default: []
+  },
+})
+
+const emit = defineEmits(["choiceMade"])
+
+</script>
+
+<style scoped>
+
+</style>
