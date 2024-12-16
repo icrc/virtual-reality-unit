f2eed4150127446303a4ed86884ad50bd44f4445
Jon Randy <jonrandy@gmail.com>
2024-12-16 13:43:11 +0700

    Remove 'Test' main menu link

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
         <li><RouterLink to="/">Home</RouterLink></li>
         <li><RouterLink to="/view">View</RouterLink></li>
         <li><RouterLink to="/edit">Edit</RouterLink></li>
-        <li><RouterLink to="/test">Test</RouterLink></li>
+        <!-- <li><RouterLink to="/test">Test</RouterLink></li> -->
         <li><RouterLink to="/help">Help</RouterLink></li>
       </menu>
     </nav>
