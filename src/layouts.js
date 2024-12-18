435b44ca1e823bcd4372968b2df8c4d4b2958c09
Jon Randy <jonrandy@gmail.com>
2024-12-18 18:31:45 +0700

    Some initial work towards layouts

---
+++ src/layouts.js
@@ -0,0 +1,33 @@
+// Layout config and defaults
+
+export const DEFAULT_LAYOUT = 'basic'
+
+export const layouts = {
+	// basic layout (default)
+	basic: {
+		name: 'Basic',
+		thumbnail: '',
+		options: {
+			colour_button_1: {
+				name: "Button 1 colour",
+				type: "colour",
+			},
+			colour_button_2: {
+				name: "Button 2 colour",
+				type: "colour",
+			},
+			colour_button_3: {
+				name: "Button 3 colour",
+				type: "colour",
+			},
+			colour_button_4: {
+				name: "Button 4 colour",
+				type: "colour",
+			},
+			background_colour: {
+				name: "Background colour",
+				type: "colour",
+			}
+		}
+	}
+}
