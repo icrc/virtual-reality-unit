508903f5b72591aa39256b5b86a38ac0a56e9e5f
Jon Randy <jonrandy@gmail.com>
2024-12-20 10:38:53 +0700

    Add some notes on layout config editing

--- src/layouts.js
+++ src/layouts.js
@@ -31,3 +31,20 @@ export const layouts = {
 		}
 	}
 }
+
+
+/*
+
+Types can be:
+
+colour - a colour, obviously (bring up a picker - make sure transparency is available too)
+font - a simple choice between a few fonts? (Check with Ryan)
+[Object] - a way of choosing more complex bits of css:
+	{
+		"css_var_name" : {
+			"css_var_value": "human readable description",
+			"10px 20px 30px 20px": "Margin style 1"
+		}
+	}
+
+*/
