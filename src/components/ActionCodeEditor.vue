f71066684d10044b541ef7e3be958479cec270d6
Jon Randy <jonrandy@gmail.com>
2025-01-13 18:12:10 +0700

    Add a component for editing ActionCode

---
+++ src/components/ActionCodeEditor.vue
@@ -0,0 +1,17 @@
+<!-- Action Code Editor Component -->
+<template>
+  <span>
+    <input placeholder="Action code" type="text" v-model="actionCode" />
+    <button><icon type="edit-3" class="icon button-icon" title="Edit action code" /></button>
+  </span>
+</template>
+
+<script></script>
+
+<script setup>
+import Icon from "vue-feather"
+
+const actionCode = defineModel()
+</script>
+
+<style scoped></style>
