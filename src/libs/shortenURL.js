e9e897326430998050dd5bdb222c253b64036739
Jon Randy <jonrandy@gmail.com>
2025-03-26 18:52:43 +0700

    Make the URL shortener capable of using fallbacks

--- src/libs/shortenURL.js
+++ src/libs/shortenURL.js
@@ -5,7 +5,7 @@
  *
  * @type       {Object}
  */
-export const IS_GD = {
+const IS_GD = {
 	name: "is.gd",
 	async shorten(url) {
 		let res
@@ -21,15 +21,40 @@ export const IS_GD = {
 }
 
 /**
+ * API for the v.gd url shortener
+ *
+ * @type       {Object}
+ */
+const V_GD = {
+	name: "v.gd",
+	async shorten(url) {
+		let res
+		try {
+			res = await fetch(`https://v.gd/create.php?format=json&url=${encodeURIComponent(url)}`)
+			if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`)
+			res = await res.json()
+			return res.errorcode ? [null, res.errormessage] : [res.shorturl]
+		} catch (error) {
+			return [null, error.message]
+		}
+	},
+}
+
+/**
  * Create a URL shortener function
  *
- * @param      {Object}    helper  The helper API
+ * @param      {Object}    helpers  The helper APIs to try
  * @return     {Function}  { description_of_the_return_value }
  */
-export const urlShortener = helper => async url => {
-	const [shortenedURL, error] = await helper.shorten(url)
-	if (!shortenedURL) console.log(`Error whilst shortening URL with '${helper.name}':`, error)
+export const urlShortener = (...helpers) => async url => {
+	let shortenedURL, error
+	for (const helper of helpers) {
+		console.log(`Attempting to shorten URL with '${helper.name}`)
+		;[shortenedURL, error] = await helper.shorten(url)
+		if (shortenedURL) break
+	}
+	if (!shortenedURL) console.log(`Error whilst shortening URL`)
 	return shortenedURL
 }
 
-export default urlShortener(IS_GD)
+export default urlShortener(v_GD, IS_GD)
