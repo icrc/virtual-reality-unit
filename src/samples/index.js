e1ce5f8a208c37b97f168a8119919f995f885394
Jon Randy <jonrandy@gmail.com>
2025-04-11 11:44:44 +0700

    Adds a basic quiz sample
    Adds a new quiz sample to showcase the application's capabilities.
    
    The quiz includes timed questions, score tracking, and data download functionality.

--- src/samples/index.js
+++ src/samples/index.js
@@ -3,8 +3,10 @@ import blast from "./blast.json"
 import layouttest from "./layouttest.json"
 import multimp4 from "./multimp4.json"
 import playthru from "./playthru.json"
+import quiz from "./quiz.json"
 
 export default [
+	quiz,
 	actionevents,
 	blast,
 	layouttest,
