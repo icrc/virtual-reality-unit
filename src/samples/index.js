47860bdb638c7da7036db300ba4be2196b8119de
Jon Randy <jonrandy@gmail.com>
2025-04-15 13:25:24 +0700

    Adds branching sample project
    Adds a new sample project that demonstrates scene branching functionality, showcasing both immediate branching using "gotoScene" and end-of-scene branching using "setNextScene".

--- src/samples/index.js
+++ src/samples/index.js
@@ -1,15 +1,7 @@
-import actionevents from "./actionevents.json"
-import blast from "./blast.json"
-import layouttest from "./layouttest.json"
-import multimp4 from "./multimp4.json"
-import playthru from "./playthru.json"
 import quiz from "./quiz.json"
+import branching from "./branching.json"
 
 export default [
 	quiz,
-	actionevents,
-	blast,
-	layouttest,
-	multimp4,
-	playthru,
+	branching,
 ]
